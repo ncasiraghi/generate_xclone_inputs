7b62e64859b3951f081781d27be7c85f2bd0b055
nicola <casiraghinicola@gmail.com>
2019-11-13 17:14:30 +0100

    add bsub unit mode

---
+++ bsub_get_xclone.sh
@@ -0,0 +1,16 @@
+#!/bin/sh 
+#BSUB -J xclone_input
+#BSUB -n 20
+#BSUB -q long
+#BSUB -e xclone_input.log 
+#BSUB -o xclone_input.txt 
+
+module load R/3.6.0 bedtools/2.24.0 bedops/2.4.14
+
+single_cells_cn="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/single_cells_cn.txt"
+unit="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/haploblocks.bed"
+phased_snps="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/all.vcf"
+outdir="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/mode_SP/unit_hb_blocks"
+cores=20
+
+Rscript /icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/get_xclone_input.R $single_cells_cn $unit $phased_snps $outdir $cores
