2f3211384fb78b1bad7a6f4c1bb6cb1650630267
nicola <casiraghinicola@gmail.com>
2019-11-15 12:30:09 +0100

    bsub added

---
+++ studio/bsub_studio_xclone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+#BSUB -J xclone_input
+#BSUB -n 50
+#BSUB -q long
+#BSUB -e xclone_input.log 
+#BSUB -o xclone_input.txt 
+
+module load R/3.6.0
+
+Rscript /icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/studio_xclone_input.R
