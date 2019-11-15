4dfb40cd79bdc751dcd13b04952d5abc27436652
nicola <casiraghinicola@gmail.com>
2019-11-15 12:30:31 +0100

    add DP and AD per each SNP

--- studio/studio_xclone_input.R
+++ studio/studio_xclone_input.R
@@ -3,48 +3,51 @@ library( parallel )
 library( Matrix )
 library( data.table )
 
-setwd("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/")
+setwd("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/rna/")
+cores <- 50
 
 ## GTseq
-cellsnp <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/single_cell_integration_dna_rna/scase/cellSNP_test/scDNA_data/sample_3/sparseVCF/"
+cellsnp <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/single_cell_integration_dna_rna/scase/cellSNP_test/scRNA_data/sample_3/sparseVCF"
 dp.mtx.file  <- file.path(cellsnp,"cellSNP.tag.DP.mtx")
+ad.mtx.file  <- file.path(cellsnp,"cellSNP.tag.AD.mtx")
 samples.file <- file.path(cellsnp,"cellSNP.samples.tsv")
 snps.file <- file.path(cellsnp,"cellSNP.base.vcf.gz")
-units.file <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/xclone_inputs/GTseq/STP-PDX/mode_RP/unit_haplotype_blocks/xci_lane1DNAA10_sequence.cbs.nochr.bed"
 
-getStatTab <- function(dp.mtx.file,units.file,samples.file){
-  
-  dp.mtx <- readMM(file = dp.mtx.file)
-  units <- fread(input = units.file,data.table = FALSE,stringsAsFactors = FALSE)
-  snps <- fread(input = snps.file,data.table = FALSE,stringsAsFactors = FALSE,skip = 1,select = c(1,2))
-  snps[,1] <- gsub(snps[,1],pattern = "chr",replacement = "")
-  snps <- unite(snps,col = SNP,seq(1,2),sep = ":",remove=TRUE)
-  
-  samples <- readLines(samples.file)
-  
-  units <- unite(units,col = UNIT,seq(6,8),sep = ":",remove=TRUE)
-  out <- split(units, f = units[,6])
-  
-  getDP <- function(i,out,snps,dp.mtx){
-    u <- out[[i]]
-    u <- unite(u,col = SNP,seq(1,2),sep = ":",remove=TRUE)
-    x <- dp.mtx[which(snps$SNP %in% unique(u$SNP)),,drop=FALSE]
-    if(nrow(x)>0){
-      tab <- cbind(rowSums(x,na.rm = F), round(rowMeans(x,na.rm = F)),3)
-      return(tab)
-    } else {
-      return(NULL)
-    }
-  }
-  m <- mclapply(seq(length(out)),getDP,out=out,snps=snps,dp.mtx=dp.mtx,mc.cores = 3)
-  names(m) <- names(out)
-  
-  save(out,m,file = "snps_DP_count_per_unit.RData",compress = TRUE)
-  
+xci.files <- list.files("/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/xclone_inputs/GTseq/STP-PDX/mode_RP/unit_haplotype_blocks",pattern = "xci_",full.names = TRUE)
+
+dp.mtx <- readMM(file = dp.mtx.file)
+ad.mtx <- readMM(file = ad.mtx.file)
+snps <- fread(input = snps.file,data.table = FALSE,stringsAsFactors = FALSE,skip = 1,select = c(1,2),verbose = FALSE)
+snps[,1] <- gsub(snps[,1],pattern = "chr",replacement = "")
+snps <- unite(snps,col = SNP,seq(1,2),sep = ":",remove=TRUE)
+
+samples <- readLines(samples.file)
+samples <- gsub(samples,pattern = ".Aligned.sortedByCoord.out.bam",replacement = "")
+
+getDP <- function(i,xci.files,snps,dp.mtx){
+  xc <- xci.files[i]
+  m <- fread(xc,data.table = F)
+  m <- unite(m,col = SNP,seq(1,2),sep = ":",remove=TRUE)
+  s <- gsub(basename(xc),pattern = "xci_lane1DNA|_sequence.cbs.nochr.bed",replacement = "")
+  dpc <- data.frame(SNP=snps$SNP,
+                    SNP_DP=as.numeric(dp.mtx[,which(samples == s)]),
+                    SNP_AD=as.numeric(ad.mtx[,which(samples == s)]),
+                    stringsAsFactors = F)
+  m <- merge(m,dpc,by = "SNP",all.x = TRUE)
+  write.table(m,file = gsub(basename(xc),pattern = "xci_",replacement = "xci_dp_"),col.names = T,row.names = F,sep = "\t",quote = F)
 }
 
+mclapply(seq(length(xci.files)),getDP,xci.files=xci.files,snps=snps,dp.mtx=dp.mtx,mc.cores = cores)
+
+# plot snps ASR distribution
 
 
+
+
+
+# select the most covered SNPs and select units
+
+if( FALSE ){
 
 # scDNA
 if(F){
@@ -136,3 +139,5 @@ for(x in unique(m$variable)){
   abline(h = 0.5,v = 0.5,col="red",lwd=0.4)
 }
 
+
+}
