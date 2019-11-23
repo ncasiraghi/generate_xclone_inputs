3be9861cac7c49321f94ab61ab9b9abbbb650f03
nicola <casiraghinicola@gmail.com>
2019-11-23 18:28:30 +0100

    organize outputs and png

--- studio/studio_xclone_input.R
+++ studio/studio_xclone_input.R
@@ -2,9 +2,14 @@ library( tidyr )
 library( parallel )
 library( Matrix )
 library( data.table )
+library( vioplot )
+library( VennDiagram )
 
 cores <- 10
 
+# generate data 
+if(FALSE){
+  
 omic <- "rna"
 
 # GenomicUnit <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/xclone_inputs/GTseq/STP-PDX/mode_SP/unit_cn_blocks/"
@@ -63,10 +68,11 @@ getDP <- function(i,xci.files,snps,dp.mtx){
 
 mclapply(seq(length(xci.files)),getDP,xci.files=xci.files,snps=snps,dp.mtx=dp.mtx,mc.cores = cores)
 
+}
 
 ## [ functions ]
 
-DataFilter <- function(i,bed,minDP=1,maxDP=Inf,minAD=0,cn=NA,maxCN=5){
+DataFilter <- function(i,bed,minDP=2,maxDP=Inf,minAD=0,cn=NA,maxCN=5){
   message(i)
   x <- fread(bed[i],data.table = FALSE)
   x <- x[which(x$SNP_DP >= minDP & x$SNP_DP <= maxDP),]
@@ -84,20 +90,6 @@ DataFilter <- function(i,bed,minDP=1,maxDP=Inf,minAD=0,cn=NA,maxCN=5){
   }
 }
 
-
-# SNP analysis
-if( FALSE ){
-
-#bed <- list.files("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/dna_unit_genes",pattern = "\\.bed$",full.names = T)
-bed <- list.files("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/rna_unit_genes",pattern = "\\.bed$",full.names = T)
-  
-df.all <- do.call(rbind,mclapply(seq(length(bed)),DataFilter,bed=bed,cn=2,mc.cores = cores))
-
-summary(df.all$COPY_NUMBER)
-quantile(df.all$SNP_DP)
-quantile(df.all$SNP_DP,probs = seq(0,1,0.01))
-
-
 plotres <- function(df,title){
   
   boxplot(df$SNP_DP,df$SNP_AD,outline = F,names = c("DP","AD"),ylab="n. reads",main=title)
@@ -110,33 +102,97 @@ plotres <- function(df,title){
   
 }
 
-par(pty="s",mfrow=c(1,2))
+# SNP analysis
 
-# check in diploid segments
-minDP <- quantile(df.all$SNP_DP,probs = 0.99)
-minDP <- 20
+bed.dna <- list.files("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/dna_unit_genes",pattern = "\\.bed$",full.names = T)
+bed.rna <- list.files("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/rna_unit_genes",pattern = "\\.bed$",full.names = T)
 
-df <- do.call(rbind,mclapply(seq(length(bed)),DataFilter,bed=bed,minDP=minDP,maxDP=Inf,minAD=0,cn=2,mc.cores = cores))
+df.all.dna <- do.call(rbind,mclapply(seq(length(bed.dna)),DataFilter,minDP=1,bed=bed.dna,cn=NA,mc.cores = cores))
+df.all.rna <- do.call(rbind,mclapply(seq(length(bed.rna)),DataFilter,minDP=1,bed=bed.rna,cn=NA,mc.cores = cores))
 
-summary(df$COPY_NUMBER)
-summary(df$SNP_DP)
-summary(df$SNP_AD)
+m <- merge(data.frame(table(df.all.dna$CELL_ID),stringsAsFactors = F),
+           data.frame(table(df.all.rna$CELL_ID),stringsAsFactors = F),by = "Var1",suffixes = c('_dna','_rna'))
 
-plotres(df,title = "SNPs in CN = 2")
+png("studio/png/Plot1.png",width = 14,height = 7,units = 'in', res = 300)
+
+layout(matrix(c(1,1,1,3,3,
+                2,2,2,3,3),2,5,byrow = T))
+
+barplot(m$Freq_dna,col = 'grey60',border = 'grey60',names.arg = m$Var1,cex.names = .6,las=2,main='n. of SNPs per cell in scDNA')
+barplot(m$Freq_rna,col = 'grey60',border = 'grey60',names.arg = m$Var1,cex.names = .6,las=2,main='n. of SNPs per cell in scRNA')
+vioplot(m$Freq_dna,m$Freq_rna,names = c("scDNA", "scRNA"),ylab="n. of SNPs per cell",pchMed = 20,col="white",rectCol = "white",colMed = "black",main=paste(median(m$Freq_dna),median(m$Freq_rna),sep = ' | '))
+
+dev.off()
+
+png("studio/png/Plot2.png",width = 14,height = 7,units = 'in', res = 300)
+
+par(pty='s',mfrow=c(1,2))
+
+boxplot(df.all.dna$SNP_DP,df.all.rna$SNP_DP,names = c("scDNA", "scRNA"),ylab="DP",outline=F,varwidth = T,main=paste(median(df.all.dna$SNP_DP),median(df.all.rna$SNP_DP),sep = ' | '))
+vioplot(df.all.dna$SNP_DP,df.all.rna$SNP_DP,names = c("scDNA", "scRNA"),pchMed = 20,ylab="DP",col="white",rectCol = "white",colMed = "black")
+
+dev.off()
 
 
-# check in segments with CN = 1
-df <- do.call(rbind,mclapply(seq(length(bed)),DataFilter,bed=bed,minDP=minDP,maxDP=Inf,minAD=0,cn=1,mc.cores = cores))
+dfm <- merge(x = df.all.dna[,c(1,5:7,10:14)],df.all.rna[,c(1,5:7,10:14)],by = c('SNP','CELL_ID','COPY_NUMBER',"GENE_CHROM","GENE_START","GENE_END"),all = FALSE,suffixes = c('_dna','_rna'))
+dfm <- unite(dfm,col = UNIT,seq(4,6),sep = ":",remove=TRUE)
 
-summary(df$COPY_NUMBER)
-summary(df$SNP_DP)
-summary(df$SNP_AD)
+png("studio/png/Plot3.png",width = 14,height = 7,units = 'in', res = 300)
 
-plotres(df,title = "SNPs in CN = 1")
+layout(matrix(c(1,1,1,1,2,2,
+                1,1,1,1,2,2,
+                1,1,1,1,3,3,
+                1,1,1,1,3,3),4,6,byrow = T))
 
+barplot(sort(table(dfm$CELL_ID),decreasing = T),cex.names = 0.6,las=2,xlab='cells',ylab='n. of SNPs covered both in DNA and RNA',col = 'grey60',border = 'grey60')
+abline(h = median(table(dfm$CELL_ID)),lwd=0.6)
+boxplot(dfm$SNP_DP_dna,dfm$SNP_DP_rna,names = c("scDNA", "scRNA"),ylab="DP",outline=F,varwidth = T,main=paste(median(dfm$SNP_DP_dna),median(dfm$SNP_DP_rna),sep = ' | '))
+vioplot(dfm$SNP_DP_dna,dfm$SNP_DP_rna,names = c("scDNA", "scRNA"),pchMed = 20,ylab="DP",col="white",rectCol = "white",colMed = "black")
+
+dev.off()
+
+png("studio/png/Plot4.png",width = 15,height = 7,units = 'in', res = 300)
+
+layout(matrix(c(1,1,2,2,3,
+                1,1,2,2,3),2,5,byrow = T))
+
+k <- dfm[which(dfm$COPY_NUMBER==2),]
+
+plot(k$SNP_DP_dna,k$SNP_DP_rna,pch=20,col='grey60',xlab='DP in DNA',ylab='DP in RNA',main=paste('n. SNPs = ',nrow(k),' in CN = 2'))
+
+plot(abs(0.5-k$SNP_ASR_dna),abs(0.5-k$SNP_ASR_rna),pch=20,col='grey60',xlab='|0.5-(AD/DP)| in DNA',ylab='|0.5-(AD/DP)| in RNA')
+
+vioplot(abs(0.5-k$SNP_ASR_dna),abs(0.5-k$SNP_ASR_rna),names = c("scDNA", "scRNA"),pchMed = 20,ylab="|0.5-(AD/DP)|",col="white",rectCol = "white",colMed = "black")
+
+dev.off()
+
+d.dp <- c()
+d.ad <- c()
+r.dp <- c()
+r.ad <- c()
+
+for( u in unique(dfm$UNIT[which(dfm$COPY_NUMBER==2)]) ){
+  d.dp <- c(d.dp, sum(dfm$SNP_DP_dna[which(dfm$UNIT == u)]))
+  d.ad <- c(d.ad, sum(dfm$SNP_AD_dna[which(dfm$UNIT == u)]))
+  
+  r.dp <- c(r.dp, sum(dfm$SNP_DP_rna[which(dfm$UNIT == u)]))
+  r.ad <- c(r.ad, sum(dfm$SNP_AD_rna[which(dfm$UNIT == u)]))
 }
 
-# Gene analysis
+png("studio/png/Plot5.png",width = 15,height = 7,units = 'in', res = 300)
+
+layout(matrix(c(1,1,2,2,3,
+                1,1,2,2,3),2,5,byrow = T))
+
+plot(d.dp,r.dp,pch=20,col='grey60',xlab='DP in DNA',ylab='DP in RNA',main=paste('n. genes = ',length(d.dp),' in CN = 2'))
+
+plot(abs(0.5-(d.ad/d.dp)),abs(0.5-(r.ad/r.dp)),pch=20,col='grey60',xlab='|0.5-(AD/DP)| in DNA',ylab='|0.5-(AD/DP)| in RNA')
+
+vioplot(abs(0.5-(d.ad/d.dp)),abs(0.5-(r.ad/r.dp)),names = c("scDNA", "scRNA"),pchMed = 20,ylab="|0.5-(AD/DP)|",col="white",rectCol = "white",colMed = "black")
+
+dev.off()
+
+# compute by genes
 
 GenomicUnit <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/xclone_inputs/GTseq/STP-PDX/mode_SP/unit_genes/"
 xci.files <- list.files(GenomicUnit,pattern = "xci_",full.names = TRUE)
@@ -162,160 +218,74 @@ for(i in seq(length(xci.files))){
 cn1 <- rowSums(tab == 1)/ncol(tab)
 cn2 <- rowSums(tab == 2)/ncol(tab)
 
-par(pty='s')
-col <- rep("grey60",length(cn1))
 
 selected_genes <- intersect(which(cn1 > 0.4 & cn1 < 0.6),which(cn2 > 0.4 & cn2 < 0.6))
 
+png("studio/png/Plot6.png",width = 7,height = 7,units = 'in', res = 300)
+
+par(pty='s')
+
+col <- rep("grey60",length(cn1))
 col[selected_genes] <- "orangered"
 
 plot(cn1,cn2,pch=20,
      xlab = "fraction of cells with this gene in CN = 1",
      ylab = "fraction of cells with this gene in CN = 2",ylim=c(0,1),xlim=c(0,1),
      col=col,main=paste("selected genes = ",length(selected_genes)))
+abline(h = c(0.4,0.6),v = c(0.4,0.6),lwd=0.4)
+
+dev.off()
 
 
-#bed <- list.files("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/dna_unit_genes",pattern = "\\.bed$",full.names = T)
-#bed <- list.files("/icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/studio/rna_unit_genes",pattern = "\\.bed$",full.names = T)
-
-df <- do.call(rbind,mclapply(seq(length(bed)),DataFilter,bed=bed,minDP=1,cn=c(1,2),mc.cores = cores))
-
-summary(df$COPY_NUMBER)
-summary(df$SNP_DP)
-summary(df$SNP_AD)
-
-df <- unite(df,col = UNIT,seq(5,7),sep = ":",remove=TRUE)
-
-genes_cn1 <- c()
-genes_cn2 <- c()
-for(gene in tab$UNIT[selected_genes]){
-  message(gene)
-  u <- df[which(df$UNIT == gene),,drop=F]
-  if(nrow(u) > 0){
-    summaryGene <- function(u,cn){
-      g <- u[which(u$COPY_NUMBER == cn),,drop=FALSE]
-      if(nrow(g) > 0){
-        this <- data.frame(UNIT=unique(g$UNIT),
-                           CN=unique(g$COPY_NUMBER),
-                           SNPS=nrow(g),
-                           CELLS=length(unique(g$CELL_ID)),
-                           DP=sum(g$SNP_DP),
-                           AD=sum(g$SNP_AD),
-                           ASR=sum(g$SNP_AD)/sum(g$SNP_DP),stringsAsFactors = F)
-        return(this)
-      } else {
-        return(NULL)
+cate <- function(bed, selected_genes,minDP){
+  
+  df <- do.call(rbind,mclapply(seq(length(bed)),DataFilter,bed=bed,minDP=minDP,cn=c(1,2),mc.cores = cores))
+  df <- unite(df,col = UNIT,seq(5,7),sep = ":",remove=TRUE)
+  
+  genes_cn1 <- c()
+  genes_cn2 <- c()
+  for(gene in tab$UNIT[selected_genes]){
+    message(gene)
+    u <- df[which(df$UNIT == gene),,drop=F]
+    if(nrow(u) > 0){
+      summaryGene <- function(u,cn){
+        g <- u[which(u$COPY_NUMBER == cn),,drop=FALSE]
+        if(nrow(g) > 0){
+          this <- data.frame(UNIT=unique(g$UNIT),
+                             CN=unique(g$COPY_NUMBER),
+                             SNPS=nrow(g),
+                             CELLS=length(unique(g$CELL_ID)),
+                             DP=sum(g$SNP_DP),
+                             AD=sum(g$SNP_AD),
+                             ASR=sum(g$SNP_AD)/sum(g$SNP_DP),stringsAsFactors = F)
+          return(this)
+        } else {
+          return(NULL)
+        }
       }
+      
+      genes_cn1 <- rbind(genes_cn1,summaryGene(u=u,cn=1))
+      genes_cn2 <- rbind(genes_cn2,summaryGene(u=u,cn=2))
     }
-    
-    genes_cn1 <- rbind(genes_cn1,summaryGene(u=u,cn=1))
-    genes_cn2 <- rbind(genes_cn2,summaryGene(u=u,cn=2))
   }
+  
+  gcn <- merge(x = genes_cn1,genes_cn2,by = "UNIT",all = FALSE,suffixes = c("_cn1","_cn2"))
+  
+  return(gcn)
+  
 }
 
-gcn <- merge(x = genes_cn1,genes_cn2,by = "UNIT",all = FALSE,suffixes = c("_cn1","_cn2"))
+gcn.dna <- cate(bed.dna,selected_genes,minDP = 1)
+gcn.rna <- cate(bed.rna,selected_genes,minDP = 1)
 
-# Add boxplots to a scatterplot
-library(vioplot)
-
-par(pty="s",mfrow=c(1,2))
-plot(abs(0.5-gcn$ASR_cn1),abs(0.5-gcn$ASR_cn2),xlab="abs(0.5-ASR) in CN=1",ylab="abs(0.5-ASR) in CN=2",xlim=c(0,0.5),ylim=c(0,0.5))
-vioplot(abs(0.5-gcn$ASR_cn1),abs(0.5-gcn$ASR_cn2),names = c("CN = 1", "CN = 2"),ylab="abs(0.5-ASR)",col="white",rectCol = "white",colMed = "black")
-#vioplot(gcn$DP_cn1,gcn$DP_cn2,names = c("CN = 1", "CN = 2"),ylab="DP",col="white",rectCol = "white",colMed = "black")
-mtext("scRNA", side=3, outer=TRUE, line=-3)
-
-# old
-if( FALSE ){
-
-# scDNA
-if(F){
-ad.dna <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/xclone/data/processed/STP_G&T/scDNA/for_nicola/ad.csv",header = TRUE,stringsAsFactors = FALSE)
-dp.dna <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/xclone/data/processed/STP_G&T/scDNA/for_nicola/dp.csv",header = TRUE,stringsAsFactors = FALSE)
-block.dna <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/xclone/data/processed/STP_G&T/scDNA/for_nicola/block_cnv_per_cell.csv",header = TRUE,stringsAsFactors = FALSE)
-block.dna <- unite(block.dna,col = group,seq(1,3),sep = ":",remove=TRUE)
-ad.dna$group <- block.dna$group
-dp.dna$group <- block.dna$group
-
-# scRNA
-ad.rna <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/xclone/data/processed/STP_G&T/scRNA/for_nicola/ad.csv",header = TRUE,stringsAsFactors = FALSE)
-dp.rna <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/xclone/data/processed/STP_G&T/scRNA/for_nicola/dp.csv",header = TRUE,stringsAsFactors = FALSE)
-block.rna <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/xclone/data/processed/STP_G&T/scRNA/for_nicola/block_cnv_per_cell.csv",header = TRUE,stringsAsFactors = FALSE)
-block.rna <- unite(block.rna,col = group,seq(1,3),sep = ":",remove=TRUE)
-ad.rna$group <- block.rna$group
-dp.rna$group <- block.rna$group
-
-# keep only intersected blocks
-keep <- intersect(block.dna$group,block.rna$group)
-
-block.dna <- block.dna[which(block.dna$group %in% keep),]
-block.rna <- block.rna[which(block.rna$group %in% keep),]
-
-ad.dna <- melt(ad.dna[which(ad.dna$group %in% keep),],id=c("group"))
-ad.rna <- melt(ad.rna[which(ad.rna$group %in% keep),],id=c("group"))
-ad.dna$variable <- gsub(ad.dna$variable,pattern = "_ad",replacement = "")
-ad.rna$variable <- gsub(ad.rna$variable,pattern = "_ad",replacement = "")
-
-ad <- merge(ad.dna,ad.rna,by = c("group","variable"),suffixes = c("_dna","_rna"))
-colnames(ad) <- gsub(colnames(ad),pattern = "value_",replacement = "AD_")
-head(ad)
-
-dp.dna <- melt(dp.dna[which(dp.dna$group %in% keep),],id=c("group"))
-dp.rna <- melt(dp.rna[which(dp.rna$group %in% keep),],id=c("group"))
-dp.dna$variable <- gsub(dp.dna$variable,pattern = "_dp",replacement = "")
-dp.rna$variable <- gsub(dp.rna$variable,pattern = "_dp",replacement = "")
-
-dp <- merge(dp.dna,dp.rna,by = c("group","variable"),suffixes = c("_dna","_rna"))
-colnames(dp) <- gsub(colnames(dp),pattern = "value_",replacement = "DP_")
-head(dp)
-
-cn <- melt(block.dna, id=c("group"))
-colnames(cn)[3] <- "COPY_NUMBER"
-
-df <- merge(cn,ad,by = c("group","variable"))
-df <- merge(df,dp,by = c("group","variable"))
-df$ASR_dna <- df$AD_dna/df$DP_dna
-df$ASR_rna <- df$AD_rna/df$DP_rna
-}
-
-m <- df[which(df$COPY_NUMBER %in% c(1:3)),]
-m <- m[grep(m$group,pattern = "^3:"),]
-
-barplot(table(m$COPY_NUMBER))
-
-m <- m[which(m$DP_dna > 0 & m$DP_rna > 0),]
-
-par(pty="s",mfrow=c(3,2))
-boxplot(DP_dna ~ COPY_NUMBER,data = m,varwidth=TRUE,outline=FALSE)
-boxplot(DP_rna ~ COPY_NUMBER,data = m,varwidth=TRUE,outline=FALSE)
-
-boxplot(AD_dna ~ COPY_NUMBER,data = m,varwidth=TRUE,outline=FALSE)
-boxplot(AD_rna ~ COPY_NUMBER,data = m,varwidth=TRUE,outline=FALSE)
-
-boxplot(ASR_dna ~ COPY_NUMBER,data = m,varwidth=TRUE,outline=FALSE)
-abline(h = 0.5)
-boxplot(ASR_rna ~ COPY_NUMBER,data = m,varwidth=TRUE,outline=FALSE)
-abline(h = 0.5)
-
-##
-
-m <- m[which(m$COPY_NUMBER == 2),]
+png("studio/png/Plot7.png",width = 7,height = 7,units = 'in', res = 300)
 
 par(pty="s",mfrow=c(2,2))
 
-rbPal <- colorRampPalette(c('#edf8b1','#225ea8'))
-m$Col <- rbPal(10)[as.numeric(cut(log(m$DP_dna),breaks = 10))]
+plot(abs(0.5-gcn.dna$ASR_cn1),abs(0.5-gcn.dna$ASR_cn2),xlab="abs(0.5-ASR) in CN=1",ylab="abs(0.5-ASR) in CN=2",xlim=c(0,0.5),ylim=c(0,0.5),main='DNA')
+vioplot(abs(0.5-gcn.dna$ASR_cn1),abs(0.5-gcn.dna$ASR_cn2),names = c("CN = 1", "CN = 2"),ylab="abs(0.5-ASR)",col="white",rectCol = "white",colMed = "black",pchMed = 20,main='DNA')
 
-plot(m$ASR_dna,m$ASR_rna,xlim=c(0,1),ylim=c(0,1),pch = 20,col = m$Col)
+plot(abs(0.5-gcn.rna$ASR_cn1),abs(0.5-gcn.rna$ASR_cn2),xlab="abs(0.5-ASR) in CN=1",ylab="abs(0.5-ASR) in CN=2",xlim=c(0,0.5),ylim=c(0,0.5),main='RNA')
+vioplot(abs(0.5-gcn.rna$ASR_cn1),abs(0.5-gcn.rna$ASR_cn2),names = c("CN = 1", "CN = 2"),ylab="abs(0.5-ASR)",col="white",rectCol = "white",colMed = "black",pchMed = 20,main='RNA')
 
-plot(density(m$ASR_dna),col="orangered",lwd=4,xlab = "ASR",main="")
-lines(density(m$ASR_rna),col="black",lwd=4)
-
-par(pty="s",mfrow=c(3,3))
-for(x in unique(m$variable)){
-  a <- m[which(m$variable == x),]
-  plot(a$ASR_dna,a$ASR_rna,main=x,xlim=c(0,1),ylim=c(0,1))
-  abline(h = 0.5,v = 0.5,col="red",lwd=0.4)
-}
-
-
-}
+dev.off()
