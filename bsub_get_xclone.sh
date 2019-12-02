bcebd936d12f1f8939cad9e85734f12a52f65f42
nicola <casiraghinicola@gmail.com>
2019-12-02 14:27:11 +0100

    example for window bins

--- bsub_get_xclone.sh
+++ bsub_get_xclone.sh
@@ -8,9 +8,9 @@
 module load R/3.6.0 bedtools/2.24.0 bedops/2.4.14
 
 single_cells_cn="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/single_cells_cn.txt"
-unit="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/haploblocks.bed"
+unit="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/mode_SP/unit_w10Mb/sliding.windows.10Mb.nochr.bed"
 phased_snps="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/all.vcf"
-outdir="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/mode_SP/unit_hb_blocks"
+outdir="/icgc/dkfzlsdf/analysis/B260/users/n790i/hipo_K08K/mode_SP/unit_w10Mb"
 cores=20
 
 Rscript /icgc/dkfzlsdf/analysis/B260/users/n790i/generate_xclone_inputs/get_xclone_input.R $single_cells_cn $unit $phased_snps $outdir $cores
